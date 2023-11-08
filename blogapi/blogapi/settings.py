4e5109381f404f1302122d60ae418cfa0bf03fe8
benkivuva <benkivuvak5@gmail.com>
2023-11-08 16:07:10 +0300

    Add pagination

--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -56,9 +56,11 @@ SWAGGER_SETTINGS = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'authentication.backends.JWTAuthentication',
-    )
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 MIDDLEWARE = [
